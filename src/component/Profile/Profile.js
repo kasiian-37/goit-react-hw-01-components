91ac48f90196e62560cb767866b8e546069dde83
kasiian irina <karamio09@gmail.com>
2020-12-29 14:53:49 +0200

    Create Profile.js

---
+++ src/component/Profile/Profile.js
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+
+import s from '../Profile/Profile.module.css';
+
+function Profile({avatar,name,tag,location,stats}) {
+    return(<div className={s.profile}>
+    <div className={s.description}>
+      <img
+        src={avatar}
+        alt="Аватар пользователя"
+        className={s.avatar} width='240'
+      />
+      <p className="name">{name}</p>
+      <p className="tag">@{tag}</p>
+      <p className="location">{location}</p>
+    </div>
+  
+    <ul className={s.stats}>
+      <li >
+        <span className={s.label}>Followes</span>
+        <span className={s.quantity}>{stats.followers}</span>
+      </li>
+      <li>
+        <span className={s.label}>Views</span>
+        <span className={s.quantity}>{stats.views}</span>
+      </li>
+      <li>
+        <span className={s.label}>Likes</span>
+        <span className={s.quantity}>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>);
+    
+}
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number).isRequired,
+}
+export default Profile;
