a0e1fb389a3dee3e3ece1c55567bbabb3dcf7178
kasiian irina <karamio09@gmail.com>
2021-01-03 14:33:10 +0200

    Create FriendList.js

---
+++ src/component/Friends/FriendList.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import FriendListItem from './FriendListItem';
+import s from './FriendList.module.css';
+
+function FriendList({friends}) {
+ return(
+
+<ul className={s.friendList}>
+  {friends.map(friend => (<FriendListItem props = {friend} key ={friend.id} />))}
+ 
+</ul>);
+}
+
+FriendList.propTypes ={
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+}
+export default FriendList;
