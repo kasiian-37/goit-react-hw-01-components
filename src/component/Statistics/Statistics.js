4c399d96d39df40d1bc4af7d3cf22640bc51979f
kasiian irina <karamio09@gmail.com>
2021-01-03 14:33:35 +0200

    Update Statistics.js

--- src/component/Statistics/Statistics.js
+++ src/component/Statistics/Statistics.js
@@ -2,30 +2,40 @@
 // stats - массив объектов содержащих информацию о элементе статистики. Может иметь произвольное кол-во элементов.
 // Цвет фона элемента статистики в оформлении можно пропустить, либо создать функцию для генерации случайного цвета.
 import PropTypes from "prop-types";
+import s from './Statistics.module.css';
+
+function rundomColor() {
+  return Math.floor(Math.random() * Math.floor(255));
+}
+function ColorRandom() {
+  return `rgb(${rundomColor()}, ${rundomColor()}, ${rundomColor()})`;
+}
 
 function Statistics({title,stats}) {
  return(   
+<section className={s.statistics}>
+{title && <h2 className={s.title}>{title}</h2>}
 
-<section className="statistics">
-  <h2 className="title">Upload stats</h2>
-
-  <ul className="stat-list">
-    <li className="item">
-      <span className={stats.label}>.docx</span>
-      <span className="percentage">4%</span>
+  <ul className= {s.statList}>
+    {stats.map(stat =>(
+    <li className={s.item}
+      key={stat.id}
+      style={{ backgroundColor: ColorRandom() }}>
+      <span className={s.label} >{stat.label}:</span>
+      <span className={s.percentage} >{stat.percentage}%</span>
     </li>
-    <li className="item">
-      <span className="label">.mp3</span>
-      <span className="percentage">14%</span>
-    </li>
-    <li className="item">
-      <span className="label">.pdf</span>
-      <span className="percentage">41%</span>
-    </li>
-    <li className="item">
-      <span className="label">.mp4</span>
-      <span className="percentage">12%</span>
-    </li>
+    ))}
   </ul>
 </section>);
 }
+Statistics.propTypes ={
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    label: PropTypes.string,
+    stats:PropTypes.number,
+  })).isRequired,
+
+}
+
+export default Statistics;
